3c83d59fb5df78660c138169aa567a5d0501290f
ahmedsheikh27 <932ahmedsheikh@gmail.com>
2023-07-06 22:51:30 +0500

    Completed Responsive Ui

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-
+import './weather.css'
 const Weather = () => {
-const [weather, setWeather] = useState(null);
+const [weatherData, setWeatherData] = useState(null);
 const [city, setCity] =  useState('');
 
 const seeWeather = async () => {
@@ -10,17 +10,43 @@ try{
     const apiKey = '8e424da8ce62044e51fd423e567f4c88';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     const response =await axios.get(apiUrl);
-    setWeather(response.data);
+    setWeatherData(response.data);
 } catch (error) {
 console.error(error);
 }
 };
 
+const getWeatherIcon = (iconCode) => {
+    const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+    return <img src={iconUrl} alt="Weather Icon" />;
+  };
+
+
 
   return (
-    <div>
+    <div className='container'>
+<h1> Weather App</h1>
+        <div className='search'>
         <input 
-        placeholder='Search City' />
+        type='text'
+        placeholder='Search City'
+        value={city}
+        onChange={(e)=>setCity(e.target.value)}
+        />
+         <button onClick={seeWeather}>
+            Search
+         </button>
+        </div>
+         {weatherData && (
+        <div className='weather-details'>
+          <h2> {weatherData.name} </h2>
+          <p className='temp'> {weatherData.main.temp}°C</p>
+          <div className='icon'>
+          {getWeatherIcon(weatherData.weather[0].icon)}
+          </div>
+          <p className='descript'> {weatherData.weather[0].description}</p>
+        </div>
+      )}
       
     </div>
   )
